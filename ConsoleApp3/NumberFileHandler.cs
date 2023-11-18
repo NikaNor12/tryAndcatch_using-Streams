60b24756d05957785d8354bfa9f07d0988a577cf
nika <nika@DESKTOP-5RG29Q9>
2023-11-18 19:33:51 +0400

    Add project files.

---
+++ ConsoleApp3/NumberFileHandler.cs
@@ -0,0 +1,49 @@
+﻿namespace ConsoleApp3
+{
+    internal class NumberFileHandler
+    {
+        public static void ProcessFile(string filePath)
+        {
+            int sum = 0;
+            string outputFilePath = @"D:\" + Path.GetFileNameWithoutExtension(filePath) + "_2.txt";
+
+            StreamReader reader = new StreamReader(filePath);
+
+            StreamWriter writer2 = new StreamWriter(outputFilePath);
+
+            int i = 1;
+            string? line;
+
+            try
+            {
+                while ((line = reader.ReadLine()) != null)
+                {
+                    if (int.TryParse(line, out int number))
+                    {
+                        sum += number;
+                        line = $"{i++}.{line}";
+                    }
+
+                    else
+                    {
+                        line = $"{i++}.{line}";
+                    }
+
+                    writer2.WriteLine($"{line}");
+                    Console.WriteLine($"{line}");
+                }
+
+                writer2.WriteLine($"Sum of numbers -> {sum}");
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine($"{ex.Message}");
+            }
+
+            Console.WriteLine($"Sum of numbers {sum}");
+            reader.Close();
+            writer2.Close();
+        }
+    }
+}
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {       
+            string FilePath = @"D:\viri.txt";
+            StreamWriter writer = new StreamWriter(FilePath);
+
+            writer.WriteLine("Nika");
+            writer.WriteLine("Elene");
+            writer.WriteLine("Gio");
+            writer.WriteLine("Gela");
+            writer.WriteLine("Dgnachvi");
+            writer.WriteLine(52);
+            writer.WriteLine(34);
+            writer.WriteLine(1);
+            writer.WriteLine(86);
+            writer.WriteLine(33);
+            writer.Close();
+
+
+            //  helper მეთოდის გამოძახება NumberFileHandler-დან.
+            NumberFileHandler.ProcessFile(FilePath);
+        }
+    }
+}
